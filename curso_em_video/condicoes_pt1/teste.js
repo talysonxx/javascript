b3f2ee982152e200b868fc2bf4f8251d87f46d8a
Talyson André <talysonoficial12@gmail.com>
2021-03-27 11:10:26 -0300

    update

---
+++ curso_em_video/condicoes_pt1/teste.js
@@ -0,0 +1,11 @@
+// console.log('O concole executou numa boa')
+
+var vel = Number.parseFloat(40)
+
+if(vel > 60) {
+    console.log(`Você ultrapasou o limite de velocidade`)
+}else {
+    console.log(`Tá de boa`)
+}
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)
