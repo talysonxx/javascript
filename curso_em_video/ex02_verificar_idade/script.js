5e007c5bde74119e524f6e80d1ef24225a26675a
Talyson André <talysonoficial12@gmail.com>
2021-03-29 10:49:37 -0300

    update

--- curso_em_video/ex02_verificar_idade/script.js
+++ curso_em_video/ex02_verificar_idade/script.js
@@ -3,36 +3,56 @@ window.document.querySelector('input#botao').addEventListener('click', verificar
 function verificar() {
     var ano = new Date().getFullYear()
     var ano_formulario = window.document.querySelector('input#nascimento').value
-    var resultado = window.document.querySelector('p#resultado')
-    //var img = window.document.createElement('img') // criar elemento com a tag img
-    //img.setAttribute('id', 'foto') // atribuir id a img com nome foto
-    var imagem = window.document.querySelector('img#imagem')
+    var resultado = window.document.querySelector('div#resultado')
+    var img = window.document.createElement('img') // criar elemento com a tag img
+    img.setAttribute('id', 'imagem') // atribuir id a img com nome foto
+    //img.setAttribute('src', `${gênero == 'homem' ? '_imagens/foto-bebe-m.png' : '_imagens/foto-bebe-f.png'}`)
 
-    if (ano_formulario.lenght == 0 || ano_formulario > ano) {
+    if (ano_formulario <= 0 || ano_formulario > ano) {
         window.alert('Digite um ano válido')
     } else {
         var sexo = window.document.getElementsByName('sexo')
         var idade = ano - ano_formulario
-        var gênero = sexo[0].checked ? 'homem' : 'mulher'
-        
-        if (idade >= 0 && idade < 10) {
-            // criança
-            //imagem.src = `${gênero == 'homem' ? '_imagens/foto-bebe-m.png' : '_imagens/foto-bebe-f.png'}`
-            imagem.setAttribute('src', `${gênero == 'homem' ? '_imagens/foto-bebe-m.png' : '_imagens/foto-bebe-f.png'}`)
-            resultado.innerHTML = `Detectamos uma criança com ${idade} ${idade == 1 ? 'ano' : 'anos'}`
-        } else if (idade < 21) {
-            // jovem
-            imagem.src = `${gênero == 'homem' ? '_imagens/foto-jovem-m.png' : '_imagens/foto-jovem-f.png'}`
-            resultado.innerHTML = `Detectamos ${gênero == 'homem' ? 'um' : 'uma'} jovem com ${idade} anos`
-        } else if (idade < 50) {
-            // adulto
-            imagem.src = `${gênero == 'homem' ? '_imagens/foto-adulto-m.png' : '_imagens/foto-adulto-f.png'}`
-            resultado.innerHTML = `Detectamos ${gênero == 'homem' ? 'um adulto' : 'uma adulta'} com ${idade} anos`
+
+        if (sexo[0].checked) {
+            // homem
+            if (idade >= 0 && idade < 10) {
+                // bebe homem
+                resultado.innerHTML = `Detectamos um bebê com ${idade} ${idade == 1 ? 'ano' : 'anos'}`
+                img.setAttribute('src', '_imagens/foto-bebe-m.png')
+            } else if (idade < 21) {
+                // jovem homem
+                resultado.innerHTML = `Detectamos um jovem com ${idade} anos`
+                img.setAttribute('src', '_imagens/foto-jovem-m.png')
+            } else if (idade < 65) {
+                resultado.innerHTML = `Detectamos um adulto com ${idade} anos`
+                // adulto homem
+                img.setAttribute('src', '_imagens/foto-adulto-m.png')
+            } else {
+                resultado.innerHTML = `Detectamos um idoso com ${idade} anos`
+                // idoso homem
+                img.setAttribute('src', '_imagens/foto-idoso-m.png')
+            }
         } else {
-            // idoso
-            imagem.src = `${gênero == 'homem' ? '_imagens/foto-idoso-m.png' : '_imagens/foto-idoso-f.png'}`
-            resultado.innerHTML = `Detectamos ${gênero == 'homem' ? 'um idoso' : 'uma idosa'} com ${idade} anos`
+            // mulher
+            if (idade >= 0 && idade < 10) {
+                // bebe mulher
+                resultado.innerHTML = `Detectamos um bebê com ${idade} ${idade == 1 ? 'ano' : 'anos'}`
+                img.setAttribute('src', '_imagens/foto-bebe-f.png')
+            } else if (idade < 21) {
+                // jovem mulher
+                resultado.innerHTML = `Detectamos uma jovem com ${idade} anos`
+                img.setAttribute('src', '_imagens/foto-jovem-f.png')
+            } else if (idade < 65) {
+                // adulto mulher
+                resultado.innerHTML = `Detectamos uma adulta com ${idade} anos`
+                img.setAttribute('src', '_imagens/foto-adulto-f.png')
+            } else {
+                // idoso mulher
+                resultado.innerHTML = `Detectamos uma idosa com ${idade} anos`
+                img.setAttribute('src', '_imagens/foto-idoso-f.png')
+            }
         }
     }
-    // resultado.appendChild(imagem) - adicionar o elemento imagem  
+    resultado.appendChild(img) // adicionar o elemento imagem embaixo do resultado
 }
