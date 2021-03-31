0ec5237b8394e3c056673bb0a48f5a3e415747c7
Talyson André <talysonoficial12@gmail.com>
2021-03-30 21:57:33 -0300

    update

---
+++ curso_em_video/repeticao_while/script.js
@@ -0,0 +1,15 @@
+var cont = 1
+
+/*while (cont <= 6) {
+    console.log(cont)
+    cont++
+}*/
+/*do {
+    console.log(cont)
+    cont++
+} while (cont <= 6);*/
+
+do {
+    var num = window.prompt('Digite o número 10:')
+} while (num != 10);
+window.alert('おめでとう！')
